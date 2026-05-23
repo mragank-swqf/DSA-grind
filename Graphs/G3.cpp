44d18c25cfe7c34b29872cea49b81d813cd4aff2
mragank-swqf <mragank001@gmail.com>
2026-05-23 16:30:49 +0530

    Learnt cycle detection in undirected graph

--- Graphs/G3.cpp
+++ Graphs/G3.cpp
@@ -1,4 +1,7 @@
-//Cycle Detection in an undirected graph
+//Cycle Detection in an undirected graph using BFS and DFS
+//To detect cycle in an undirected graph, idea is same for both
+//BFS and DFS, its about tracking the visited nodes as well as parent check
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -14,7 +17,7 @@ vector<vector<int>> undirected_graph() {
     } return adjlist;
 }
 
-bool cycle_detect(vector<vector<int>>adj_list) {
+bool cycle_detect_BFS(vector<vector<int>>&adj_list) {
     queue<pair<int, int>> q;
     q.push({1, -1}); int n = adj_list.size(); 
     vector<int> visited(n+1); visited[1] = 1;
@@ -29,13 +32,24 @@ bool cycle_detect(vector<vector<int>>adj_list) {
         } 
     } return false;
 }
+bool cycle_detect_DFS(int root, int parent, vector<vector<int>>&adj_list, vector<int>&visited) {
+    if (visited[root]) return true;
+    visited[root] = 1;
+    for (int i=0; i<adj_list[root].size(); i++) {
+        if (adj_list[root][i] == parent) continue;
+        if (!visited[adj_list[root][i]]) { if (cycle_detect_DFS(adj_list[root][i], root, adj_list, visited)) return true; }
+        else return true;
+    } return false;
+}
 int main() {
     int t; cin >> t;
     while (t--) {
         int d; cin >> d;
         if (d==0) {
             vector<vector<int>> adj_list = undirected_graph();
-            cout << cycle_detect(adj_list) << endl;
+            int n = adj_list.size();
+            vector<int> visited(n+1, 0);
+            cout << cycle_detect_DFS(1, 0, adj_list, visited) << endl;
         }
     }
     return 0;
